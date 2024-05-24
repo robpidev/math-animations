41ef959db4a49b3a79b97dafd28a94ed9ad9d213
Rober <robertdrakenfor@gmail.com>
2024-05-23 22:00:48 -0500

    Props end

---
+++ vectors/p14_m10.py
@@ -0,0 +1,45 @@
+from manim import *
+
+
+class Prop10(Scene):
+    def construct(self):
+        self.wait()
+        self.next_section(skip_animations=True)
+        self.next_section(skip_animations=False)
+        prop = MathTex(
+            r"\text{10. }",
+            r"1 \mathbf{u}",
+            "=", r"\mathbf{u}",
+            r", \quad \forall\mathbf{u} \in \mathbb{R}"
+        )
+
+        for mo in prop:
+            self.play(Write(mo))
+
+        self.wait()
+        self.play(prop.animate.scale(0.8).to_edge(UP + LEFT))
+        self.wait()
+
+        eq = MathTex(
+            r"3 \mathbf{u} + \mathbf{u}",
+            "=", "3 \mathbf{u} + 1\mathbf{u}",
+            "=", "4 \mathbf{u}"
+        )
+
+        self.play(Write(eq[0]))
+        self.wait()
+        self.play(Write(eq[1]))
+
+        self.play(TransformMatchingShapes(
+            eq[0].copy(), eq[2],
+            transform_mismatches=True
+        ))
+
+        self.wait()
+        self.play(Write(eq[3]))
+        self.play(TransformMatchingShapes(
+            eq[2].copy(), eq[4],
+            transform_mismatches=True
+        ))
+
+        self.wait()
